5fc99f3d589949df2c3748a8f2f0a494a3ee5770
Shahriar Fardows <shahriarfardows@gmail.com>
2025-03-31 22:43:36 +0600

    date picker fix

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -2,7 +2,8 @@ import axios from "axios"
 
 // Create an axios instance with baseURL
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://snowfye-server-production.up.railway.app",
 })
 
 const useAxios = () => {
